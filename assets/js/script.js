c813970d92efb2fc566e940c06689114200031bb
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2024-12-08 21:45:53 +0100

    create navigation bar

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const menuButton = document.getElementById("menu-button");
+const navBar = document.getElementById("nav-bar");
+const icon = document.querySelector(".fa-solid")
+
+function toggleNavbar(){
+ console.log("toggeled");
+ navBar.classList.toggle("active");
+ if(navBar.classList.contains("active")){
+    menuButton.classList.remove("left-[190px]")
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+    
+ }
+ else {
+    menuButton.classList.add("left-[190px]")
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+ }
+}
+
+menuButton.addEventListener("click",toggleNavbar);
